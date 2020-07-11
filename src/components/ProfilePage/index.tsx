a50c695bb0b1f00f229f0e61793334dfa6c2e686
viniciusBotelho625 <58275652+viniciusBotelho625@users.noreply.github.com>
2020-07-11 19:27:24 -0300

    Perfil do usuário

---
+++ src/components/ProfilePage/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import { 
+        Container, 
+        Banner, Avatar, 
+        ProfileData, 
+        LocationIcon,
+        CakeIcon, 
+        Followage,
+        } from './style';
+
+const ProfilePage: React.FC = () => {
+  return (
+      <Container>
+          <Banner>
+              <Avatar />
+          </Banner>
+
+          <ProfileData>
+            {/* <EditButton outlined>Editar Perfil</EditButton> */}
+
+            <h1>Vinicius Botelho</h1>
+            <h2>@poxa_botelho</h2>
+
+            <p>
+                Support at <a href="https://www.interplayers.com.br/">@Interplayers</a>
+            </p>
+            <ul>
+                <li>
+                    <LocationIcon />
+                    São Paulo, Brasil
+                </li>
+                <li>
+                    <CakeIcon />
+                    Nascido(a) em 16 de março de 2000
+                </li>
+
+                <Followage>
+                    <span>
+                        seguindo <strong>94</strong>
+                    </span>
+                    <span>
+                        <strong>2000 </strong> seguidores
+                    </span>
+                </Followage>
+            </ul>
+          </ProfileData>
+      </Container>
+  )
+}
+
+export default ProfilePage;
--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import ProfilePage from '../ProfilePage';
+
 import { Container, 
         Header, 
         BackIcon, 
@@ -25,7 +27,7 @@ const main: React.FC = () => {
               </ProfileInfo>
           </Header>
 
-          {/* <ProfilePage />  */}
+          <ProfilePage /> 
 
           <BottomMenu>
               <HomeIcon className="active"/>
